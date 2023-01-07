e27ae82acc10da7a95965c246ebe6af35ce8eb7f
Nicolás Londoño <nicolas.londono@veevart.com>
2023-01-06 21:08:25 -0500

    Add: versions routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 
-const app = express();
+const v1Router = require("./v1/routes");
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send("<h1>I'm alive dad</h2>");
-});
+app.use("/api/v1/", v1Router);
 
 app.listen(PORT, () => {
     console.log(`API is listening on port ${PORT}`);
