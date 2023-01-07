fdaabfa778ea5b979730a64ffab03b9ccc6a57b3
Nicolás Londoño <nicolas.londono@veevart.com>
2023-01-06 20:38:21 -0500

    Add: hello world express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("<h1>I'm alive dad</h2>");
+});
+
+app.listen(PORT, () => {
+    console.log(`API is listening on port ${PORT}`);
+});
